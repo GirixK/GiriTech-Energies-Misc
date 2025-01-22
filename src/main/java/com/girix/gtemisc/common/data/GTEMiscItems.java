50cad8212eaf74d9dc4f3f80fbe9349bdb7ba740
GirixK <GirixK@users.noreply.github.com>
2025-01-22 20:33:16 +0100

    Add missing tools + bandaid fix

--- src/main/java/com/girix/gtemisc/common/data/GTEMiscItems.java
+++ src/main/java/com/girix/gtemisc/common/data/GTEMiscItems.java
@@ -14,6 +14,9 @@ public class GTEMiscItems {
     public static final ItemEntry<Item> SINGLE_USE_SAW = makeTool("saw");
     public static final ItemEntry<Item> SINGLE_USE_FILE = makeTool("file");
     public static final ItemEntry<Item> SINGLE_USE_WRENCH = makeTool("wrench");
+    public static final ItemEntry<Item> SINGLE_USE_WIRECUTTER = makeTool("wire_cutter");
+    public static final ItemEntry<Item> SINGLE_USE_CROWBAR = makeTool("crowbar");
+    public static final ItemEntry<Item> SINGLE_USE_SOFT_MALLET = makeTool("mallet");
 
     // Casts
     public static final ItemEntry<Item> SINGLE_USE_HAMMER_CAST = makeCast("hammer");
@@ -21,23 +24,23 @@ public class GTEMiscItems {
     public static final ItemEntry<Item> SINGLE_USE_SAW_CAST = makeCast("saw");
     public static final ItemEntry<Item> SINGLE_USE_FILE_CAST = makeCast("file");
     public static final ItemEntry<Item> SINGLE_USE_WRENCH_CAST = makeCast("wrench");
-
-    //TODO add crowbar, soft mallet, and wire cutters
+    public static final ItemEntry<Item> SINGLE_USE_WIRECUTTER_CAST = makeCast("wire_cutter");
+    public static final ItemEntry<Item> SINGLE_USE_CROWBAR_CAST = makeCast("crowbar");
+    public static final ItemEntry<Item> SINGLE_USE_SOFT_MALLET_CAST = makeCast("mallet");
 
 
     private static ItemEntry<Item> makeCast (String toolType) {
         return GTEMISC_REGISTRATE.item("single_use_" + toolType + "_cast", Item::new)
-                .lang(toolType.substring(0,1).toUpperCase() + toolType.substring(1) + " Cast")
+                .lang((toolType.equals("wire_cutter") ? "Wirecutters" : toolType.substring(0,1).toUpperCase() + toolType.substring(1) ) + " Cast")
                 .register();
     }
 
     private static ItemEntry<Item> makeTool (String toolType) {
         return GTEMISC_REGISTRATE.item("single_use_" + toolType, Item::new)
-                .lang("Single-use " + toolType.substring(0,1).toUpperCase() + toolType.substring(1))
+                .lang("Single-use " + (toolType.equals("wire_cutter") ? "Wirecutters" : toolType.substring(0,1).toUpperCase() + toolType.substring(1) ))
                 .tag(TagUtil.createItemTag("tools/" + toolType + (toolType.equals("wrench") ? "es" : "s") , false))
                 .register();
     }
-
     public static void init() {}
 
 }
