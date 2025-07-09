2222716deff80f197447d330b186dfc5bef739ec
GirixK <GirixK@users.noreply.github.com>
2025-07-09 06:15:08 +0200

    creative tab change

--- src/main/java/com/girix/gtemisc/common/data/GTEMiscCreativeModeTabs.java
+++ src/main/java/com/girix/gtemisc/common/data/GTEMiscCreativeModeTabs.java
@@ -1,10 +1,12 @@
 package com.girix.gtemisc.common.data;
 
+import com.girix.gtemisc.GTEMiscConfig;
 import com.girix.gtemisc.GiriTechMisc;
 
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.common.data.GTCreativeModeTabs;
 
+import com.gregtechceu.gtceu.common.data.GTItems;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 
@@ -17,7 +19,8 @@ public class GTEMiscCreativeModeTabs {
     public static RegistryEntry<CreativeModeTab> GTE_MISC = GTEMISC_REGISTRATE.defaultCreativeTab(GiriTechMisc.MOD_ID,
             builder -> builder.displayItems(new GTCreativeModeTabs.RegistrateDisplayItemsGenerator(GiriTechMisc.MOD_ID, GTEMISC_REGISTRATE))
                     .title(Component.literal("GiriTech Energies: Misc"))
-                    .icon(() -> GTEMiscMachines.TOOL_CASTING_MACHINE[GTValues.HV].asStack())
+                    .icon(() -> (GTEMiscConfig.INSTANCE.features.enableSingleUseTools) ?
+                            GTEMiscMachines.TOOL_CASTING_MACHINE[GTValues.HV].asStack() : GTItems.SHAPE_EMPTY.asStack())
                     .build())
             .register();
     static {
