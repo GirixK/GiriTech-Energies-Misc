ec10926b0bcee6cf76f2bf3fcc907e82c9278a0b
GirixK <GirixK@users.noreply.github.com>
2025-08-03 15:29:27 +0200

    spotless of shame

--- src/main/java/com/girix/gtemisc/data/lang/GTEMiscConfigurationLang.java
+++ src/main/java/com/girix/gtemisc/data/lang/GTEMiscConfigurationLang.java
@@ -1,18 +1,12 @@
 package com.girix.gtemisc.data.lang;
 
 import com.girix.gtemisc.GTEMiscConfig;
-import com.girix.gtemisc.GiriTechMisc;
 
 import com.tterrag.registrate.providers.RegistrateLangProvider;
-import dev.toma.configuration.Configuration;
-import dev.toma.configuration.config.format.ConfigFormats;
-import dev.toma.configuration.config.value.ConfigValue;
 import dev.toma.configuration.config.value.IConfigValue;
 import dev.toma.configuration.config.value.IHierarchical;
-import dev.toma.configuration.config.value.ObjectValue;
 
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class GTEMiscConfigurationLang {
@@ -21,8 +15,9 @@ public class GTEMiscConfigurationLang {
         final Set<String> added = new HashSet<>();
         GTEMiscConfig.INTERNAL_INSTANCE.values()
                 .forEach((value) -> addTranslation(provider, added, value));
-                //Configuration.registerConfig(GTEMiscConfig.class, ConfigFormats.yaml()).getValueMap());
+        // Configuration.registerConfig(GTEMiscConfig.class, ConfigFormats.yaml()).getValueMap());
     }
+
     private static void addTranslation(RegistrateLangProvider provider, Set<String> added, IConfigValue<?> value) {
         var id = value.getId();
         if (added.add(id)) {
