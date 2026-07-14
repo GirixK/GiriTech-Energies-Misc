96298f4ed99156072bf4825165a6be93b547a3d5
GirixK <GirixK@users.noreply.github.com>
2026-07-14 23:57:55 +0200

    clean up unused code

--- src/main/java/com/girix/gtemisc/common/data/GTEMiscMachines.java
+++ src/main/java/com/girix/gtemisc/common/data/GTEMiscMachines.java
@@ -4,28 +4,16 @@ import com.girix.gtemisc.GTEMiscConfig;
 
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTValues;
-import com.gregtechceu.gtceu.api.blockentity.BlockEntityCreationInfo;
 import com.gregtechceu.gtceu.api.data.RotationState;
-
 import com.gregtechceu.gtceu.api.machine.MachineDefinition;
-import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.SimpleTieredMachine;
-import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
-import com.gregtechceu.gtceu.api.registry.registrate.MachineBuilder;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
-
 import com.gregtechceu.gtceu.common.mui.GTSingleblockMachinePanels;
-import it.unimi.dsi.fastutil.ints.Int2IntFunction;
-
-import java.util.Locale;
-import java.util.function.BiFunction;
 
 import static com.girix.gtemisc.GiriTechMisc.GTEMISC_REGISTRATE;
-
 import static com.gregtechceu.gtceu.api.GTValues.*;
 import static com.gregtechceu.gtceu.common.data.machines.GTMachineUtils.*;
-import static com.gregtechceu.gtceu.utils.FormattingUtil.toEnglishName;
 
 public class GTEMiscMachines {
 
@@ -37,7 +25,7 @@ public class GTEMiscMachines {
 
     static {
         if (GTEMiscConfig.INSTANCE.features.enableSingleUseTools) {
-            TOOL_CASTING_MACHINE = registerTieredMachines(GTEMISC_REGISTRATE,"tool_casting_machine",
+            TOOL_CASTING_MACHINE = registerTieredMachines(GTEMISC_REGISTRATE, "tool_casting_machine",
                     (holder, tier) -> new SimpleTieredMachine(holder, tier, defaultTankSizeFunction), (tier, builder) -> builder
                             .recipeType(GTEMiscRecipeTypes.TOOL_CASTING_RECIPES)
                             .recipeModifier(GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.NON_PERFECT_OVERCLOCK))
@@ -53,45 +41,5 @@ public class GTEMiscMachines {
 
     }
 
-    // I want this separated so here's a comment to make it look better
-
-//    private static MachineDefinition[] registerTieredMachines(String name,
-//                                                              BiFunction<BlockEntityCreationInfo, Integer, MetaMachine> factory,
-//                                                              BiFunction<Integer, MachineBuilder<MachineDefinition, ?, ?>, MachineDefinition> builder,
-//                                                              int... tiers) {
-//        MachineDefinition[] definitions = new MachineDefinition[GTValues.TIER_COUNT];
-//        for (int tier : tiers) {
-//            var register = GTEMISC_REGISTRATE.machine(GTValues.VN[tier].toLowerCase(Locale.ROOT) + "_" + name,
-//                    holder -> factory.apply(holder, tier)).tier(tier);
-//            definitions[tier] = builder.apply(tier, register);
-//        }
-//        return definitions;
-//    }
-//
-//    public static MachineDefinition[] registerSimpleMachines(String name, GTRecipeType recipeType,
-//                                                             Int2IntFunction tankScalingFunction) {
-//        return registerSimpleMachines(name, recipeType, tankScalingFunction, ELECTRIC_TIERS);
-//    }
-//
-//    public static MachineDefinition[] registerSimpleMachines(String name, GTRecipeType recipeType) {
-//        return registerSimpleMachines(name, recipeType, defaultTankSizeFunction);
-//    }
-//
-//    public static MachineDefinition[] registerSimpleMachines(String name, GTRecipeType recipeType,
-//                                                             Int2IntFunction tankScalingFunction, int... tiers) {
-//        return registerTieredMachines(name,
-//                (holder, tier) -> new SimpleTieredMachine(holder, tier, tankScalingFunction), (tier, builder) -> builder
-//                        .langValue("%s %s %s".formatted(VLVH[tier], toEnglishName(name), VLVT[tier]))
-//                        .editableUI(SimpleTieredMachine.EDITABLE_UI_CREATOR.apply(GTCEu.id(name), recipeType))
-//                        .rotationState(RotationState.NON_Y_AXIS)
-//                        .recipeType(recipeType)
-//                        .recipeModifier(
-//                                GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.NON_PERFECT_OVERCLOCK))
-//                        .workableTieredHullModel(GTCEu.id("block/machines/" + name))
-//                        .tooltips(workableTiered(tier, GTValues.V[tier], GTValues.V[tier] * 64, recipeType,
-//                                tankScalingFunction.apply(tier), true))
-//                        .register(),
-//                tiers);
-//    }
     public static void init() {}
 }
